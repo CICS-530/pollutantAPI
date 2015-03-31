0e3f2cb6fe0ee4d37617134ae49dd424adfc3fe6
achengscode <docaholic@gmail.com>
2015-03-30 23:17:10 -0700

    removing fixtures as it was giving weird config problems

--- app/Test/Case/Controller/ReadingControllerTest.php
+++ app/Test/Case/Controller/ReadingControllerTest.php
@@ -5,15 +5,13 @@
 */
 class ReadingControllerTest extends ControllerTestCase {
 
-	public $fixtures = array('app.reading');
-
 	/**
 	* Testing the datedata method
 	*/
 	public function test_dateData() {
 		$this->testAction('/reading/dataDate/25/0', array('method' => 'get'));
 		$results = $this->vars;
-		$this->assertEquals(3, count($results["readings"]));
+		$this->assertEquals(4, count($results["readings"]));
 	}
 
 	/**
@@ -21,7 +19,8 @@ class ReadingControllerTest extends ControllerTestCase {
 	* @expectedException NotFoundException
 	*/
 	public function test_badRequest() {
-		$this->testAction('/reading/dataDate/25/3', array('method' => 'get'));
+		$this->testAction('/reading/dataDate/25/7', array('method' => 'get'));
+		debug($this->vars);
 	}
 
 
@@ -29,9 +28,9 @@ class ReadingControllerTest extends ControllerTestCase {
 	* Testing the datedata method
 	*/
 	public function test_dateBefore() {
-		$this->testAction('/reading/dataDate/25/1', array('method' => 'get'));
+		$this->testAction('/reading/dataDate/25/3', array('method' => 'get'));
 		$results = $this->vars;
-		$this->assertEquals(2, count($results["readings"]));
+		$this->assertEquals(5, count($results["readings"]));
 	}
 
 	/**
@@ -40,5 +39,7 @@ class ReadingControllerTest extends ControllerTestCase {
 	*/
 	public function test_latestData() {
 		$this->testAction('/reading/latestData/25', array('method' => 'get'));
+		$results = $this->vars;
+		$this->assertEquals(2, count($results["readings"]));
 	}
 }
