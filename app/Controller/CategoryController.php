d565102b16cb7b29285704a49e95394f258ba648
achengscode <docaholic@gmail.com>
2015-04-10 19:57:55 -0700

    adding back getAll function

--- app/Controller/CategoryController.php
+++ app/Controller/CategoryController.php
@@ -105,5 +105,46 @@ class CategoryController extends AppController {
 	
 		$this->set ('categories', $category);
 	}
+
+		/**
+	* Gets ALL categories and ALL diseases associated with the specified category.
+	* Ideally we can abstract this to a getAll method as well.
+	*/
+	
+	function getAll($id) {
+		$this->Category->recursive = -1;
+
+		if ($id == null) { 
+			throw new BadRequestException("Must supply a category ID");
+		}
+
+		$results = $this->Category->find('all', 
+			array("joins" => 
+					array(array("table" => "Categories_Diseases",
+						  "type" => "inner",
+						  "conditions" => array("Category.id = Categories_Diseases.category_id")),
+						  array("table" => "Diseases",
+								"type" => "inner",
+								"conditions" => array("Diseases.id = Categories_Diseases.diseases_id")),
+						  array("table" => "Diseases_Toxins",
+								"type" => "inner",
+								"conditions" => array("Diseases.id = Diseases_Toxins.disease_id")),
+						  array("table" => "Toxins",
+								"type" => "inner",
+								"conditions" => array("Toxins.id = Diseases_Toxins.toxin_id"))
+					),
+				   "fields" =>
+				   	array("Category.name", "Diseases.name", "Toxins.name", "Diseases_Toxins.evidence_strength"),
+				   	"order" => array("Category.name", "Diseases_Toxins.evidence_strength"),
+				   	"conditions" => array("Category.id = ". $id)
+			)
+		);
+
+		if ($this->Category->getAffectedRows() == 0 ) {
+			throw new NotFoundException("No results found!");
+		}
+
+		$this->set('allData', $results);
+	}
 	
 }
